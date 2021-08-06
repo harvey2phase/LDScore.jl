9d4d252f357a41820d13b6538f8dc015ec2e5a50
Harvey Wang <harvey@dal.ca>
2021-08-06 02:07:04 -0300

    tests pass

--- src/LDScore/abstract_regression.jl
+++ src/LDScore/abstract_regression.jl
@@ -1,14 +1,72 @@
 abstract type LD_Score_Regression end
 
 function aggregate(
-    ld_score_regression::LD_Score_Regression,
+    reg::LD_Score_Regression,
     y, x_tot, N, M_tot, intercept,
 )
-    if intercept == nothing intecept = ld_score_regression.__null_intercept__ end
+    if intercept == nothing intecept = reg.__null_intercept__ end
 end
 
 function _update_weights(
-    ld_score_regression::LD_Score_Regression,
+    reg::LD_Score_Regression,
     x_tot, w, N, M_tot, tot_agg, intercept,
 )
+    if intercept == nothing intercept = reg.__null_intercept__ end
+end
+
+function make_ld_score_regression(
+    reg::LD_Score_Regression,
+    y, x, w, N, M, n_blocks, intercept, slow, step1_ii, old_weights,
+)
+    #= TODO
+    for i in [y, x, w, M, N]:
+        try:
+            if len(i.shape) != 2:
+                raise TypeError('Arguments must be 2D arrays.')
+        except AttributeError:
+            raise TypeError('Arguments must be arrays.')
+    =#
+
+    n_snp, reg.n_annot = size(x)
+    #= TODO
+    if any(i.shape != (n_snp, 1) for i in [y, w, N]):
+        raise ValueError(
+            'N, weights and response (z1z2 or chisq) must have shape (n_snp, 1).')
+    if M.shape != (1, self.n_annot):
+        raise ValueError('M must have shape (1, n_annot).')
+    =#
+
+    M_tot = sum(M)
+    # shape should be [n_snp, 1]
+    x_tot = sum(x, dims = 2)'
+    reg.constrain_intercept = intercept != nothing
+    print("WOW")
+    print(reg.constrain_intercept)
+
+    tot_agg = aggregate(reg, y, x_tot, N, M_tot, intercept)
+    initial_w = _update_weights(
+        reg,
+        x_tot, w, N, M_tot, tot_agg, intercept,
+    )
+    Nbar = mean(N)
+    x = N .* x / Nbar
+
+    if !reg.constrain_intercept
+        x, x_tot = append_intercept(x), append_intercept(x_tot)
+        yp = y
+    else
+        yp = y - intercept
+        intercept_se = "NA"
+    end
+
+end
+
+function append_intercept(x)
+    print(x)
+    #=
+    n_row = x.shape[0]
+    intercept = np.ones((n_row, 1))
+    x_new = np.concatenate((x, intercept), axis=1)
+    return x_new
+    =#
 end
--- src/LDScore/regression.jl
+++ src/LDScore/regression.jl
@@ -17,6 +17,8 @@ mutable struct Hsq <: LD_Score_Regression
 
     constrain_intercept
     n_annot
+    intercept_se
+    twostep_filtered
     function Hsq(
         y, x, w, N, M, n_blocks, intercept, slow, step1_ii, old_weights,
     )
@@ -25,42 +27,4 @@ mutable struct Hsq <: LD_Score_Regression
         )
         return hsq
     end
-end
-
-
-function make_ld_score_regression(
-    ld_score_regression::LD_Score_Regression,
-    y, x, w, N, M, n_blocks, intercept, slow, step1_ii, old_weights,
-)
-    #= TODO
-    for i in [y, x, w, M, N]:
-        try:
-            if len(i.shape) != 2:
-                raise TypeError('Arguments must be 2D arrays.')
-        except AttributeError:
-            raise TypeError('Arguments must be arrays.')
-    =#
-
-
-    n_snp, ld_score_regression.n_annot = size(x)
-    #= TODO
-    if any(i.shape != (n_snp, 1) for i in [y, w, N]):
-        raise ValueError(
-            'N, weights and response (z1z2 or chisq) must have shape (n_snp, 1).')
-    if M.shape != (1, self.n_annot):
-        raise ValueError('M must have shape (1, n_annot).')
-    =#
-
-    M_tot = sum(M)
-    # shape should be [n_snp, 1]
-    x_tot = sum(x, dims = 2)'
-    constrain_intercept = intercept != nothing
-
-    tot_agg = aggregate(ld_score_regression, y, x_tot, N, M_tot, intercept)
-    initial_w = _update_weights(
-        ld_score_regression,
-        x_tot, w, N, M_tot, tot_agg, intercept,
-    )
-    Nbar = mean(N)
-
 end
