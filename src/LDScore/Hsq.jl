de66c116542b3608077b957d5bfb8bba7a7147c2
Harvey Wang <harvey@dal.ca>
2021-08-07 12:23:37 -0300

    moved things to appropriate files

--- src/LDScore/Hsq.jl
+++ src/LDScore/Hsq.jl
@@ -24,3 +24,27 @@ mutable struct Hsq <: LD_Score_Regression
         return hsq
     end
 end
+
+# TODO add a test (works though)
+function hsq_weights(ld, w_ld, N, M, hsq, intercept)
+    if intercept == nothing intercept = 1 end
+    hsq = max(maximum(hsq), 0.0)
+    hsq = min(hsq, 1.0)
+    ld = fmax(ld, 1.0)
+    w_ld = fmax(w_ld, 1.0)
+    c = hsq .* N ./ M
+
+    het_w = 1.0 ./ (2 .* (c .* ld .+ intercept).^2)
+    oc_w = 1.0 ./ w_ld
+    w = het_w .* oc_w
+    return w
+end
+
+function _update_weights(
+    reg::Hsq,
+    ld, w_ld, N, M, hsq, intercept,
+)
+    if intercept == nothing intercept = reg.__null_intercept__ end
+
+    return hsq_weights(ld, w_ld, N, M, hsq, intercept)
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,8 @@ using Test
 
 println("==================== NEW ===========================")
 
-# Write your tests here.
-# synthetic dataset
+# TODO begin testset ===========================================================
+
 # column vectors d = 6 * 1
 y = [[0.2061] [0.2601] [4.3514] [6.1703] [5.0221] [2.418 ]]'
 x = [[6.1388] [6.4785] [8.88] [4.8064] [3.6219] [3.7244]]'
@@ -23,25 +23,38 @@ synthetic_hsq = LDScoreJulia.Hsq(
 )
 
 synthetic_hsq.old_weights == false
-LDScoreJulia.make_ld_score_regression(
+LDScoreJulia.ld_score_regression(
     synthetic_hsq,
     y, x, w, N, M, n_blocks, intercept, slow, step1_ii, old_weights,
 )
 
-#=
 @test typeof(synthetic_hsq) == LDScoreJulia.Hsq
 @test synthetic_hsq.__null_intercept__ == 1
-@test synthetic_hsq.y == [[0.2061] [0.2601] [4.3514] [6.1703] [5.0221] [2.418 ]]'
-@test synthetic_hsq.x == [[6.1388] [6.4785] [8.88] [4.8064] [3.6219] [3.7244]]'
-@test synthetic_hsq.w == [[6.1388] [6.4785] [8.88] [4.8064] [3.6219] [3.7244]]'
-@test synthetic_hsq.N == [[592497.] [591333.] [696881.] [617319.] [687344.] [685637.]]'
-@test synthetic_hsq.M == [[1173569.]]
-@test synthetic_hsq.n_blocks == 200
-@test synthetic_hsq.intercept == nothing
-@test synthetic_hsq.slow == false
-@test synthetic_hsq.step1_ii == [[true] [true] [true] [true] [true] [true]]
-=#
+@test synthetic_hsq.y == y
+@test synthetic_hsq.x == x
+@test synthetic_hsq.w == w
+@test synthetic_hsq.N == N
+@test synthetic_hsq.M == M
+@test synthetic_hsq.n_blocks == n_blocks
+@test synthetic_hsq.intercept == intercept
+@test synthetic_hsq.slow == slow
+@test synthetic_hsq.step1_ii == step1_ii
+
+# end test set -----------------------------------------------------------------
+
+#=
+# TODO begin test set ==========================================================
+
+chisq = ones((4, 1)) .* 4
+ld = ones((4, 1))
+w_ld = ones((4, 1))
+N = 9 .* ones((4, 1))
+M = matrix((7))
+hsq = LDScoreJulia.Hsq(chisq, ld, w_ld, N, M, n_blocks=3, intercept=1)
 
 @testset "Hsq weights" begin
 
 end
+
+# end test set -----------------------------------------------------------------
+=#
