1e10b4d6c67f3f8b44149ffa804de0b5d2116c9a
Harvey Wang <harvey@dal.ca>
2021-08-07 11:38:06 -0300

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,8 @@ using Main
 using Main.LDScoreJulia
 using Test
 
+println("==================== NEW ===========================")
+
 # Write your tests here.
 # synthetic dataset
 # column vectors d = 6 * 1
@@ -10,7 +12,7 @@ x = [[6.1388] [6.4785] [8.88] [4.8064] [3.6219] [3.7244]]'
 w = [[6.1388] [6.4785] [8.88] [4.8064] [3.6219] [3.7244]]'
 N = [[592497.] [591333.] [696881.] [617319.] [687344.] [685637.]]'
 M = [[1173569.]]
-n_blocks = 200
+n_blocks = 2
 intercept = nothing
 slow = false
 step1_ii = [[true] [true] [true] [true] [true] [true]]'
@@ -19,6 +21,13 @@ old_weights = false
 synthetic_hsq = LDScoreJulia.Hsq(
     y, x, w, N, M, n_blocks, intercept, slow, step1_ii, old_weights,
 )
+
+synthetic_hsq.old_weights == false
+LDScoreJulia.make_ld_score_regression(
+    synthetic_hsq,
+    y, x, w, N, M, n_blocks, intercept, slow, step1_ii, old_weights,
+)
+
 #=
 @test typeof(synthetic_hsq) == LDScoreJulia.Hsq
 @test synthetic_hsq.__null_intercept__ == 1
@@ -32,11 +41,3 @@ synthetic_hsq = LDScoreJulia.Hsq(
 @test synthetic_hsq.slow == false
 @test synthetic_hsq.step1_ii == [[true] [true] [true] [true] [true] [true]]
 =#
-
-println("==================== NEW ===========================")
-
-synthetic_hsq.old_weights == false
-LDScoreJulia.make_ld_score_regression(
-    synthetic_hsq,
-    y, x, w, N, M, n_blocks, intercept, slow, step1_ii, old_weights,
-)
