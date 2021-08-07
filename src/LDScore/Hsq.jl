a7018774806cdb7c61528c5f1939cbbec66fd5bb
Harvey Wang <harvey@dal.ca>
2021-08-06 22:36:29 -0300

    Create Hsq.jl

---
+++ src/LDScore/Hsq.jl
@@ -0,0 +1,26 @@
+mutable struct Hsq <: LD_Score_Regression
+    y
+    x
+    w
+    N
+    M
+    n_blocks
+    intercept
+    slow
+    step1_ii
+    old_weights
+    __null_intercept__
+
+    constrain_intercept
+    n_annot
+    intercept_se
+    twostep_filtered
+    function Hsq(
+        y, x, w, N, M, n_blocks, intercept, slow, step1_ii, old_weights,
+    )
+        hsq = new(
+            y, x, w, N, M, n_blocks, intercept, slow, step1_ii, old_weights, 1,
+        )
+        return hsq
+    end
+end
